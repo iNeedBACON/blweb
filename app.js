18257b00471be7e9f821ae51c2ff88dc5f5a0f97
iNeedBACON <112415010+iNeedBACON@users.noreply.github.com>
2022-11-26 16:06:16 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const header = document.querySelector("header");
+
+let prevScrollPos = window.pageYOffset;
+
+window.onscroll = function () {
+    let currentScrollPos = window.pageYOffset;
+    prevScrollPos > currentScrollPos
+        ? header.classList.remove("scroll")
+        : header.classList.add("scroll");
+
+    prevScrollPos = currentScrollPos;
+};
